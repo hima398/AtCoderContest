7ca20d0e1afeb086bc19a6f3c3528a1a6b1f6091
hinata398 <r.hinata0398@gmail.com>
2020-11-21 10:02:22 +0900

    resolve BC182 D

--- BC182/D/main.go
+++ BC182/D/main.go
@@ -1,42 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"sort"
+	"os"
+	"strconv"
 )
 
-func Max(nums []int) int {
-	b := sort.IntSlice(nums)
-	sort.Sort(b)
-	return b[len(b)-1]
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
+}
+
+func Max(x, y int) int {
+	if x >= y {
+		return x
+	}
+	return y
 }
 
 func main() {
+	buf := make([]byte, 1024*1024)
+	sc.Buffer(buf, bufio.MaxScanTokenSize)
+	sc.Split(bufio.ScanWords)
+
 	// 1 <= n <= 200000
 	var n int
 	fmt.Scan(&n)
 	// -10 ^ 8 <= a[i] <= 10 ^8
 	a := make([]int, n)
 	for i := 0; i < n; i++ {
-		fmt.Scan(&a[i])
+		a[i] = nextInt()
 	}
 	lenA := len(a)
-	p := []int{}
-	pq := []int{}
-	q := []int{}
-	pi := 0
-	for i := 0; i < lenA; i++ {
-		pi += a[i]
-		p = append(p, pi)
-		pq = append(pq, pi)
-		q = append(q, Max(pq))
-	}
-	robot := 0
-	max := 0
-	for i := 0; i < lenA; i++ {
-		if max < robot+q[i] {
-			max = robot + q[i]
-		}
+	p := make([]int, n)
+	q := make([]int, n)
+	p[0] = a[0]
+	q[0] = Max(0, a[0])
+	robot := p[0]
+	max := q[0]
+	for i := 1; i < lenA; i++ {
+		p[i] = p[i-1] + a[i]
+		q[i] = Max(q[i-1], p[i])
+		max = Max(max, robot+q[i])
 		robot += p[i]
 	}
 	fmt.Println(max)
