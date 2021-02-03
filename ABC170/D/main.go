c1325ad5b41078ffa9d34cf4fb865dc2d9af8920
hinata398 <r.hinata0398@gmail.com>
2021-02-03 21:18:57 +0900

    solve ABC170D

--- ABC170/D/main.go
+++ ABC170/D/main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
 	"sort"
 	"strconv"
 )
-
-const Mod = 1000000007
 
 var sc = bufio.NewScanner(os.Stdin)
 
@@ -19,25 +16,6 @@ func nextInt() int {
 	return i
 }
 
-func nextString() string {
-	sc.Scan()
-	return sc.Text()
-}
-
-func Abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func Min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
 func Max(x, y int) int {
 	if x < y {
 		return y
@@ -45,117 +23,14 @@ func Max(x, y int) int {
 	return x
 }
 
-func Gcd(x, y int) int {
-	if y == 0 {
-		return x
+func SloveHonestly() int {
+	n := nextInt()
+	a := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		a[i] = nextInt()
 	}
-	return Gcd(y, x%y)
-}
 
-func Lcm(x, y int) int {
-	return x * y / Gcd(x, y)
-}
-
-func Permutation(N, K int) int {
-	v := 1
-	if 0 < K && K <= N {
-		for i := 0; i < K; i++ {
-			v *= (N - i)
-		}
-	} else if K > N {
-		v = 0
-	}
-	return v
-}
-
-func Factional(N int) int {
-	return Permutation(N, N-1)
-}
-
-func Combination(N, K int) int {
-	if K == 1 {
-		return N
-	}
-	return Combination(N, K-1) * (N + 1 - K) / K
-}
-
-func DivideSlice(A []int, K int) ([]int, []int, error) {
-
-	if len(A) < K {
-		return nil, nil, errors.New("")
-	}
-	return A[:K+1], A[K:], nil
-}
-
-type Pos struct {
-	X int
-	Y int
-}
-
-type UnionFind struct {
-	par  []int // parent numbers
-	rank []int // height of tree
-}
-
-func NewUnionFind(n int) *UnionFind {
-	if n <= 0 {
-		return nil
-	}
-	u := new(UnionFind)
-	// for accessing index without minus 1
-	u.par = make([]int, n+1)
-	u.rank = make([]int, n+1)
-	for i := 0; i <= n; i++ {
-		u.par[i] = i
-		u.rank[i] = 0
-	}
-	return u
-}
-
-func (this *UnionFind) Find(x int) int {
-	if this.par[x] == x {
-		return x
-	} else {
-		// compress path
-		// ex. Find(4)
-		// 1 - 2 - 3 - 4
-		// 1 - 2
-		//  L-3
-		//  L 4
-		this.par[x] = this.Find(this.par[x])
-		return this.par[x]
-	}
-}
-
-func (this *UnionFind) ExistSameUnion(x, y int) bool {
-	return this.Find(x) == this.Find(y)
-}
-
-func (this *UnionFind) Unite(x, y int) {
-	x = this.Find(x)
-	y = this.Find(y)
-	if x == y {
-		return
-	}
-	// raink
-	if this.rank[x] < this.rank[y] {
-		this.par[x] = y
-	} else {
-		// this.rank[x] >= this.rank[y]
-		this.par[y] = x
-		if this.rank[x] == this.rank[y] {
-			this.rank[x]++
-		}
-	}
-}
-
-func PrintUnionFind(u *UnionFind) {
-	// for debuging. not optimize.
-	fmt.Println(u.par)
-	fmt.Println(u.rank)
-}
-
-func SloveHonestly(n int, a []int) int {
 	ans := 0
 	for i := 0; i < n; i++ {
 		cantDivide := true
@@ -172,9 +47,35 @@ func SloveHonestly(n int, a []int) int {
 	return ans
 }
 
-func Solve(n int, a []int) {
+func Solve() int {
+	n := nextInt()
+	a := make([]int, n)
+	max := 0
+	for i := 0; i < n; i++ {
+		a[i] = nextInt()
+		max = Max(max, a[i])
+	}
+
+	d := make([]int, max+1)
+
 	sort.Ints(a)
-	d := make([]int, a[n-1])
+	for _, v := range a {
+		d[v]++
+		if d[v] > 1 {
+			continue
+		}
+		for i := 2 * v; i <= max; i += v {
+			d[i]++
+		}
+	}
+	//fmt.Println(d)
+	ans := 0
+	for _, v := range a {
+		if d[v] == 1 {
+			ans++
+		}
+	}
+	return ans
 }
 
 func main() {
@@ -182,10 +83,6 @@ func main() {
 	sc.Buffer(buf, bufio.MaxScanTokenSize)
 	sc.Split(bufio.ScanWords)
 
-	n := nextInt()
-	a := make([]int, n)
-	for i := 0; i < n; i++ {
-		a[i] = nextInt()
-	}
-	fmt.Println(SloveHonestly(n, a))
+	//fmt.Println(SloveHonestly())
+	fmt.Println(Solve())
 }
