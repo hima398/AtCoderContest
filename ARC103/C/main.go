3300e2d439d636a5ce5787221de7d3b127f1712a
hinata398 <r.hinata0398@gmail.com>
2021-02-11 22:44:44 +0900

    try ARC103C

--- ARC103/C/main.go
+++ ARC103/C/main.go
@@ -2,21 +2,87 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
-const Mod = 1000000007
-
 var sc = bufio.NewScanner(os.Stdin)
+
+type Item struct {
+	K int
+	V int
+}
+
+type List []Item
+
+func (l List) Len() int {
+	return len(l)
+}
+
+func (l List) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+func (l List) Less(i, j int) bool {
+	return l[i].V > l[i].V
+}
 
 func main() {
 	buf := make([]byte, 1024*1024)
 	sc.Buffer(buf, bufio.MaxScanTokenSize)
 	sc.Split(bufio.ScanWords)
 
+	n := nextInt()
+	mOdd := make(map[int]int)
+	mEven := make(map[int]int)
+	for i := 1; i <= n; i++ {
+		v := nextInt()
+		if i%2 == 1 {
+			mOdd[v]++
+		} else {
+			mEven[v]++
+		}
+	}
+	//fmt.Println(mOdd)
+	//fmt.Println(mEven)
+	var lOdd List
+	var lEven List
+	for k, v := range mOdd {
+		i := Item{k, v}
+		lOdd = append(lOdd, i)
+	}
+	for k, v := range mEven {
+		i := Item{k, v}
+		lEven = append(lEven, i)
+	}
+	sort.Sort(lOdd)
+	sort.Sort(lEven)
+
+	o1 := lOdd[0]
+	e1 := lEven[0]
+	var ans int
+	if o1.K == e1.K {
+		io := 1
+		ie := 1
+		if lOdd.Len() == 1 {
+			io = 0
+		}
+		if lEven.Len() == 1 {
+			ie = 0
+		}
+		if io == 0 && ie == 0 {
+			ans = n / 2
+		} else {
+			o2 := lOdd[io]
+			e2 := lEven[ie]
+			ans = Min(n-o1.V-e2.V, n-o2.V-e1.V)
+		}
+	} else {
+		ans = n - o1.V - e1.V
+	}
+	fmt.Println(ans)
 }
 
 func nextInt() int {
@@ -25,214 +91,9 @@ func nextInt() int {
 	return i
 }
 
-func nextString() string {
-	sc.Scan()
-	return sc.Text()
-}
-
-func Abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
 func Min(x, y int) int {
 	if x < y {
 		return x
 	}
 	return y
-}
-
-func Max(x, y int) int {
-	if x < y {
-		return y
-	}
-	return x
-}
-
-func Gcd(x, y int) int {
-	if y == 0 {
-		return x
-	}
-	return Gcd(y, x%y)
-}
-
-func Lcm(x, y int) int {
-	return x * y / Gcd(x, y)
-}
-
-func Pow(x, y, p int) int {
-	ret := 1
-	for y > 0 {
-		if y%2 == 1 {
-			ret = ret * x % p
-		}
-		y >>= 1
-		x = x * x % p
-	}
-	return ret
-}
-
-func Permutation(N, K int) int {
-	v := 1
-	if 0 < K && K <= N {
-		for i := 0; i < K; i++ {
-			v *= (N - i)
-		}
-	} else if K > N {
-		v = 0
-	}
-	return v
-}
-
-func Factional(N int) int {
-	return Permutation(N, N-1)
-}
-
-func Combination(N, K int) int {
-	if K == 1 {
-		return N
-	}
-	return Combination(N, K-1) * (N + 1 - K) / K
-}
-
-func DivideSlice(A []int, K int) ([]int, []int, error) {
-
-	if len(A) < K {
-		return nil, nil, errors.New("")
-	}
-	return A[:K+1], A[K:], nil
-}
-
-type IntQueue struct {
-	q []int
-}
-
-func NewIntQueue() *IntQueue {
-
-	return new(IntQueue)
-}
-func (this *IntQueue) Push(v int) {
-	this.q = append(this.q, v)
-}
-
-func (this *IntQueue) Pop() (int, error) {
-	if this.Size() == 0 {
-		return -1, errors.New("")
-	}
-	ret := this.q[0]
-	this.q = this.q[1:]
-	return ret, nil
-}
-
-func (this *IntQueue) Size() int {
-	return len(this.q)
-}
-
-func (this *IntQueue) PrintQueue() {
-	fmt.Println(this.q)
-}
-
-type Pos struct {
-	X int
-	Y int
-	D int
-}
-
-type Queue struct {
-	ps []Pos
-}
-
-func NewQueue() *Queue {
-	return new(Queue)
-}
-
-func (this *Queue) Push(p Pos) {
-	this.ps = append(this.ps, p)
-}
-
-func (this *Queue) Pop() *Pos {
-	if len(this.ps) == 0 {
-		return nil
-	}
-	p := this.ps[0]
-	this.ps = this.ps[1:]
-	return &p
-}
-
-func (this *Queue) Find(x, y int) bool {
-	for _, v := range this.ps {
-		if x == v.X && y == v.Y {
-			return true
-		}
-	}
-	return false
-}
-
-func (this *Queue) Size() int {
-	return len(this.ps)
-}
-
-type UnionFind struct {
-	par  []int // parent numbers
-	rank []int // height of tree
-}
-
-func NewUnionFind(n int) *UnionFind {
-	if n <= 0 {
-		return nil
-	}
-	u := new(UnionFind)
-	// for accessing index without minus 1
-	u.par = make([]int, n+1)
-	u.rank = make([]int, n+1)
-	for i := 0; i <= n; i++ {
-		u.par[i] = i
-		u.rank[i] = 0
-	}
-	return u
-}
-
-func (this *UnionFind) Find(x int) int {
-	if this.par[x] == x {
-		return x
-	} else {
-		// compress path
-		// ex. Find(4)
-		// 1 - 2 - 3 - 4
-		// 1 - 2
-		//  L-3
-		//  L 4
-		this.par[x] = this.Find(this.par[x])
-		return this.par[x]
-	}
-}
-
-func (this *UnionFind) ExistSameUnion(x, y int) bool {
-	return this.Find(x) == this.Find(y)
-}
-
-func (this *UnionFind) Unite(x, y int) {
-	x = this.Find(x)
-	y = this.Find(y)
-	if x == y {
-		return
-	}
-	// raink
-	if this.rank[x] < this.rank[y] {
-		this.par[x] = y
-	} else {
-		// this.rank[x] >= this.rank[y]
-		this.par[y] = x
-		if this.rank[x] == this.rank[y] {
-			this.rank[x]++
-		}
-	}
-}
-
-func PrintUnionFind(u *UnionFind) {
-	// for debuging. not optimize.
-	fmt.Println(u.par)
-	fmt.Println(u.rank)
 }
