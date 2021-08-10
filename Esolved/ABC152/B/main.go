d377c25405eb2f472239b1da20d368d934200012
hinata398 <r.hinata0398@gmail.com>
2021-08-10 21:38:04 +0900

    solve ABC152

---
+++ Esolved/ABC152/B/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	buf := make([]byte, 1024*1024)
+	sc.Buffer(buf, bufio.MaxScanTokenSize)
+	sc.Split(bufio.ScanWords)
+
+	a, b := nextInt(), nextInt()
+	sa, sb := "", ""
+	for i := 0; i < a; i++ {
+		sb += strconv.Itoa(b)
+	}
+	for i := 0; i < b; i++ {
+		sa += strconv.Itoa(a)
+	}
+	if sa < sb {
+		fmt.Println(sa)
+	} else {
+		fmt.Println(sb)
+	}
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
+}
